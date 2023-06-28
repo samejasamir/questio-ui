26d5afb1d15fab44fdbe26f58b6414e68460d6e2
Samir Sameja <samejasamir@hotmail.com>
2023-06-27 23:02:31 -0400

    added shell for the landing page (#3)

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.scss';
 
-function App() {
+export default function App() {
   return (
-    <div className='container'>Hello World</div>
+    <>
+        <div className='container text-center'>          
+          <div className='col'>
+            <div className='banner'>Quest.IO</div>
+          </div>          
+          <div className='col'>
+            <div className='disclaimer1'>
+              An experimental project to explore and demonstrate the usage of Artificial Intelligence in the finance vertical.
+            </div>
+          </div>
+          <div className='col'>
+            <div className='disclaimer2'>
+              All information on this page is on 'as-is' basis and none of the data and it's derivation is claimed to be accurate.
+            </div>
+          </div>
+        </div>
+    </>    
   );
 }
-
-export default App;
