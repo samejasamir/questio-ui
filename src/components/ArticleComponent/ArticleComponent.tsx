fb1a7920b8c18d1dd41b878df9b969c621e3bca3
Samir Sameja <samejasamir@hotmail.com>
2023-06-29 23:17:09 -0400

    wired the components with service (#6)

--- src/components/ArticleComponent/ArticleComponent.tsx
+++ src/components/ArticleComponent/ArticleComponent.tsx
@@ -1,18 +1,36 @@
-import React, { FC } from 'react';
+import React, { useEffect, useState } from 'react';
+import ArticlesService from '../../services/ArticlesService';
 import './ArticleComponent.scss';
 
-interface ArticleComponentProps {}
+export default function ArticleComponent(){
 
-const ArticleComponent: FC<ArticleComponentProps> = (props) => (  
-  <div className="cardContainer">
-    <div className="card-body">
-      <h5 className="card-title">Will US Slip in Recession in 2023?</h5>
-      <h6 className="card-subtitle mb-2 text-body-secondary">Source: StringZ Solutions</h6>
-      <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-      <a href="#" className="badge badge-primary">Primary</a><br/>
-      <a href="#" className="card-link">https://stringzsolutions.com/324/23/234/234sd/ff/</a>
+  const [articles, setArticles] = useState<Array<any>>([]);
+
+  useEffect(()=>{
+    ArticlesService.getAllArticles()
+    .then((res) => {
+      setArticles(res.data)
+    })
+  }, []);
+  
+  return (
+    <div className='row'>
+    {
+      articles.map((article, index)=>(        
+         <div className="cardContainer">
+          <div className="card-body">
+            <h5 className="card-title">Title not available</h5>
+            {/* <h6 className="card-subtitle mb-2 text-body-secondary">Source: {article.mediaHouse}</h6> */}
+            <p className="card-text">{article.articleSummary}</p>
+            <h6 className="badgeCategory">{article.articleCategory}</h6>
+            <h6 className="badgeSource">{article.mediaHouse}</h6><br/>
+            <a href={article.articleURL} className="card-link" target='window'>{article.articleURL}</a>
+          </div>
+        </div>
+      ))
+    }
     </div>
-  </div>
-);
+  );
 
-export default ArticleComponent;
+}
+
--- src/services/ArticlesService.ts
+++ src/services/ArticlesService.ts
@@ -4,3 +4,9 @@ import { getBaseURL } from "../config/config";
 const getAllArticles = () =>{
     return axios.get(getBaseURL("articles/all"));
 }
+
+const ArticlesService = {
+    getAllArticles
+}
+
+export default ArticlesService;
