62f7c3f5104825dfb3f66a1226ec6be2ec0db609
Samir Sameja <samejasamir@hotmail.com>
2023-07-02 22:22:06 -0400

    added title to cards (#8)

--- src/App.tsx
+++ src/App.tsx
@@ -5,23 +5,25 @@ export default function App() {
   return (
     <>
     {/* MOBILE */}
-        <div className='bannerContainerMobile'>    
-          <div className='row'>
-            <div className='col text-center'>
-              <div className='banner'>Quest.IO</div>
-            </div>            
-          </div>
-          <div className='row'>
-            <div className='col text-center'>
-              <div className='disclaimer1'>
-                An experimental project to explore and demonstrate the usage of Artificial Intelligence in the finance vertical.
-              </div>
-              <br/>
-              <div className='disclaimer2'>
-              All information on this page is sourced form public sources (Internet) and <span className='disclaimer3'>none of the data and it's derivation is claimed to be accurate.</span>
-              </div>
-              <br/>
-            </div>          
+        <div className='bannerContainerMobile'>
+          <div className='container'>
+            <div className='row'>
+              <div className='col text-center'>
+                <div className='banner'>Quest.IO</div>
+              </div>            
+            </div>
+            <div className='row'>
+              <div className='col text-center'>
+                <div className='disclaimer1'>
+                  An experimental project to explore and demonstrate the usage of Artificial Intelligence in the finance vertical.
+                </div>
+                <br/>
+                <div className='disclaimer2'>
+                All information on this page is sourced form public sources (Internet) and <span className='disclaimer3'>none of the data and it's derivation is claimed to be accurate.</span>
+                </div>
+                <br/>
+              </div>          
+            </div>
           </div>
         </div>
     {/* DESKTOP */}
