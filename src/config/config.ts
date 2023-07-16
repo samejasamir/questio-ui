3277ce29d8a9fe02293774af2b817d8522d4e403
Samir Sameja <samejasamir@hotmail.com>
2023-07-15 21:53:54 -0400

    updated prod url (#9)

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,5 +1,5 @@
 export const getBaseURL = (suffix:String) =>
 {
-    var baseUrl = process.env.NODE_ENV === "production" ? "" : "http://localhost:8080/v1";
+    var baseUrl = process.env.NODE_ENV === "production" ? "https://questioweb.azurewebsites.net/v1" : "http://localhost:8080/v1";
     return baseUrl + "/" + suffix;
 }
